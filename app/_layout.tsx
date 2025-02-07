79987aa361f2866e195118cd251436f849abb403
Isaacjbar <isaac777jb@gmail.com>
2025-02-07 17:24:55 -0600

    issue: THIS VERSION IS OK

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { StyleContextProvider } from "../core/StyleContext";
+import "../global.css"
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <StyleContextProvider>
+      <Stack />
+    </StyleContextProvider>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,56 +1,13 @@
-
-import {Text, View, Image, StyleSheet, TextInput,TouchableOpacity } from "react-native";
-import { useRouter } from "expo-router";
-import { Routes } from "../utils/routes";
-
-export default function Index() {
-  const router = useRouter();
-
+import React from "react";
+import { registerRootComponent } from "expo"; 
+import { StyleContextProvider } from "../core/StyleContext"; 
+import AuthScreen from "../pages/auth/auth"; 
+export default function App() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Image source={{uri:"https://static.vecteezy.com/system/resources/previews/022/123/337/non_2x/user-icon-profile-icon-account-icon-login-sign-line-vector.jpg"}} style={styles.image}></Image>
-      <Text style={styles.title}>Iniciar sesión</Text>
-      <TextInput style={styles.input} placeholder="Correo"></TextInput>
-      <TextInput style={styles.input} placeholder="Contraseña"></TextInput>
-      <TouchableOpacity style={styles.button} onPress={() => router.push(Routes.dashboard)}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
-    </View>
+    <StyleContextProvider>
+      <AuthScreen />
+    </StyleContextProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor:"white"
-  },
-  title: {
-    fontSize: 30,
-    color:"black"
-  },
-  image:{
-    height:100,
-    width:100
-  },
-  button:{
-    width:100,
-    backgroundColor:"black",
-    padding:7,
-    marginTop:10,
-    alignItems: "center",
-    borderRadius:5
-  },
-  buttonText:{
-    color:"white"
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  }
-});
+registerRootComponent(App);
